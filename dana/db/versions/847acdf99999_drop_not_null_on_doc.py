9b023646e921f41d76b31111e9c1ac160165441e
Rit Li <get@rit.li>
2017-05-27 10:07:50 -0700

    Parallelize import

---
+++ dana/db/versions/847acdf99999_drop_not_null_on_doc.py
@@ -0,0 +1,24 @@
+"""drop not null on doc
+
+Revision ID: 847acdf99999
+Revises: a1cb061c23b6
+Create Date: 2017-05-27 09:22:35.699560
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '847acdf99999'
+down_revision = 'a1cb061c23b6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('collections', 'doc', nullable=True)
+
+
+def downgrade():
+    op.alter_column('collections', 'doc', nullable=False)
