37922aaa2228cb1430ecbccce3e019731509e3f7
Rit Li <get@rit.li>
2017-07-05 13:50:39 -0700

    Add content/collection tree

--- dana/api/server.py
+++ dana/api/server.py
@@ -13,3 +13,18 @@ def navtree(slug):
     rows = db.session.execute(sql, dict(slug=slug))
     tree = nav(rows, Node(row=root))
     return json.dumps(tree[slug], cls=NodeEncoder)
+
+
+@app.route('/collectiontree/<slug>')
+def collectiontree(slug):
+    doc = db.session.query(Collection).get(slug).doc
+    collections = db.session.query(Collection)\
+            .filter_by(parent_slug=slug).order_by('position')
+    docs = [
+            dict(slug=c.slug, label=c.label, metadata=c.doc.get("metadata", []))
+            for c in collections
+            ]
+    # TODO merge manifests and collections into 'children'
+    doc['manifests'] = []
+    doc['collections'] = docs
+    return json.dumps(doc)
